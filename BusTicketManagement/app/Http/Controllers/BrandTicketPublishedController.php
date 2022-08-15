e0eec61705cc9461a86fb41753f992f1ca5011af
Khan Abrar Shams <Khan.abrar.shams@g.bracu.ac.bd>
2022-08-16 04:14:06 +0600

    imma bnout to take a risk, but now i am safe

--- BusTicketManagement/app/Http/Controllers/BrandTicketPublishedController.php
+++ BusTicketManagement/app/Http/Controllers/BrandTicketPublishedController.php
@@ -13,9 +13,13 @@ class BrandTicketPublishedController extends Controller
     public function BrandAddTicketSubmit(Request $req)
     {
         print_r($req->input());
-        // $data = new Brand_Ticket_Published;
-        // $data -> route_name = $req->input("RouteName");
-        // $data -> save();
-        // return redirect()->back();
+        $data = new Brand_Ticket_Published;
+        $data -> brand_ticket_from = $req->input("Start_RouteName");
+        $data -> brand_ticket_to = $req->input("Destination_RouteName");
+        $data -> brand_ticket_seat = $req->input("No_Seats");
+        $data -> brand_ticket_date = $req->input("Start_Time");
+        $data -> brand_ticket_price = $req->input("Ticket_Price");
+        $data -> save();
+        return redirect()->back();
     }
 }
